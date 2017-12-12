d0efeecb42ab8d1040e01dbcdef22c8ca18fb2ec
Roger Davenport <roger.neil.davenport@gmail.com>
2017-12-12 10:10:01 -0800

    installed fs and books.json

--- server.js
+++ server.js
@@ -3,12 +3,14 @@
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
 
 const client = new pg.Client(process.env.DATABASE_URL);
+
 client.connect();
 client.on('error', err => console.error(err));
 
@@ -17,6 +19,10 @@ app.use(cors());
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
+
+
+
 
 // PORT=3000
 // CLIENT_URL=http://localhost:8080
